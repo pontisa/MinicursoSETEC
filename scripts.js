7d654bf2ec3d90215eaf74f4f44952d5c239bdf4
Isabella Pontes <pontisa@protonmail.com>
2023-10-05 11:44:21 -0300

    commit final

--- scripts.js
+++ scripts.js
@@ -23,7 +23,7 @@ function calcularComanda() {
     porcentagemGorjeta = 0;
   }
 
-  const gorjeta = valorConta * (porcentagemGorjeta / 0);
+  const gorjeta = valorConta * (porcentagemGorjeta / 100);
 
   if (isNaN(valorConta)) {
     valorConta = 0;
@@ -76,4 +76,23 @@ function atualizarComanda() {
       "</label>";
     comandaDiv.appendChild(itemHtml);
   }
+}
+
+function limparCampos() {
+  comanda = [];
+  
+  atualizarComanda();
+
+  document.getElementById("porcentagemGorjeta").value = "";
+  document.getElementById("itensConsumidos").selectedIndex = 0;
+
+  const resultadoGorjeta = document.getElementById("resultadoGorjeta");
+  resultadoGorjeta.innerHTML = "";
+
+  fecharPopup();
+}
+
+function fecharPopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "none";
 }
